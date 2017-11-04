9d1a342a3446012cc1efdae1a52020eaafff1271
Vanadium <nvanadium@lm5000031238.local>
2017-11-04 16:41:11 -0400

    adding variables

--- main.tf
+++ main.tf
@@ -198,3 +198,17 @@ ingress {
     to_port		= 3306
     protocol		= "tcp"
     security_group 	= ["${aws_security_group.public.id}", "${aws_security_group.private.id}"]
+
+# DB
+
+resource "aws_db_instance" "db" {
+  allocated_storage	= 10
+  engine 		= "mysql"
+  engine_version	= "5.6.27"
+  instance_class	= "{var.db_instance_class}"
+  name			= "${var.dbname}"
+  username		= "${var.dbuser}"
+  password		= "${var.dbpassword}"
+  db_subnet_group_name	= "${aws_db_subnet_group.rds_subnetgroup.name}"
+  vpc_security_group_ids = ["${aws_security_group.RDS.id}"]
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,7 @@
-localip
+variable "aws_region" {}
+variable "aws_profile" {}
+variable "localip" {}
+variable "db_instance_class" {}
+variable "dbname" {}
+variable "dbuser" {}
+variable "dbpassword" {}
